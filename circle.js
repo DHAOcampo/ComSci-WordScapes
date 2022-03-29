334895f0db03a49a76f6c869416a6124f1e4b29d
SteohenTunaya <93633899+SteohenTunaya@users.noreply.github.com>
2022-03-29 04:13:11 -0700

    Add files via upload

--- circle.js
+++ circle.js
@@ -7,7 +7,10 @@ if(counter > 10){
     counter--
     document.getElementById('footer').style.transform =`scaleX(${counter})`;
     if (counter == 0){
-        document.getElementById("header").innerHTML = "GAME OVER";
+        document.getElementById('footer').remove();
+        var game = document.createElement("div");
+   game.setAttribute('class','end');
+        document.getElementById("body").appendChild(game);
     }
 }
 
@@ -35,19 +38,185 @@ function boxdeath(){
     element.remove();
 }
 
+var head =  document.getElementById("header");
 function scoregood(){
-    document.getElementById("header").innerHTML = "GOOD"
+
+   head.innerHTML = "GOOD";
+   head.classList.remove('animation'); 
+  void head.offsetWidth;
+  head.classList.add('animation');
 counter = counter + 5;
 }
 function scoreperfect(){
-    document.getElementById("header").innerHTML = "PERFECT"
+    head.innerHTML = "PERFECT";
+head.classList.remove('animation'); 
+  void head.offsetWidth;
+  head.classList.add('animation');
     counter = counter + 10;
 }
 }
 
 
+function circle1(){
 
+    var element = document.createElement("div");
+    var ring = document.createElement("div");
+    ring.setAttribute('class','ring');
+            element.setAttribute('id','element1');
+            element.setAttribute('class','circle');
+            document.getElementById("body").appendChild(element);
+            element.appendChild(ring);
+           element.addEventListener("click", scoregood);
+            timeout = setTimeout(score_perfect, 1000);
+     element.addEventListener("click",element.remove);
+     yes = setTimeout(boxdeath, 2000);
+    
+     function score_perfect(){
+    element.removeEventListener("click", scoregood);
+    element.addEventListener("click", scoreperfect);
+    }
+    
+    function boxdeath(){
+        element.remove();
+    }
+    
+    var head =  document.getElementById("header");
+    function scoregood(){
+    
+       head.innerHTML = "GOOD";
+       head.classList.remove('animation'); 
+      void head.offsetWidth;
+      head.classList.add('animation');
+    counter = counter + 5;
+    }
+    function scoreperfect(){
+        head.innerHTML = "PERFECT";
+    head.classList.remove('animation'); 
+      void head.offsetWidth;
+      head.classList.add('animation');
+        counter = counter + 10;
+    }
+    }
+    function slider(){
+    var head =  document.getElementById("header");
+        var element = document.createElement("div");
+        var ball = document.createElement("div");
+        ball.setAttribute('class','ball');
+                element.setAttribute('id','slide');
+                document.getElementById("body").appendChild(element);
+                element.appendChild(ball);
+               ball.addEventListener("mousedown", moving);
+               ball.addEventListener("mouseup",scoreok);
+               
+        yes = setTimeout(element.remove, 3000);
 
+      function moving(){
+        timeout = setTimeout(stoptheleave, 1800);
+        ball.addEventListener("mouseleave", boxdeath);
+        ball.style.transform = "translateX(500px)";
+        timeout = setTimeout(score_perfect, 2000);
+      }
+
+      function stoptheleave(){
+        element.removeEventListener("mouseleave", boxdeath);
+      }
+      function score_perfect(){
+        element.removeEventListener("mouseup",boxdeath);
+        element.addEventListener("mouseup",scoring);
+      }
+
+      function scoring(){
+        head.innerHTML = "PERFECT";
+        head.classList.remove('animation'); 
+          void head.offsetWidth;
+          head.classList.add('animation');
+            counter = counter + 10;
+            ball.remove();
+            element.remove();
+      }
+
+      function scoreok(){
+        ball.remove();
+        element.remove();
+     head.innerHTML = "Ok?";
+     head.classList.remove('animation'); 
+    void head.offsetWidth;
+    head.classList.add('animation');
+  counter = counter + 1;
+      }
+
+      function boxdeath(){
+          ball.remove();
+          element.remove();
+       head.innerHTML = "BAD";
+       head.classList.remove('animation'); 
+      void head.offsetWidth;
+      head.classList.add('animation');
+    counter = counter - 1;
+
+      }
+        }
+
+        function slider1(){
+            var head =  document.getElementById("header");
+                var element = document.createElement("div");
+                var ball = document.createElement("div");
+                ball.setAttribute('class','ball1');
+                        element.setAttribute('id','slide1');
+                        document.getElementById("body").appendChild(element);
+                        element.appendChild(ball);
+                       ball.addEventListener("mousedown", moving);
+                       ball.addEventListener("mouseup",scoreok);
+                       
+                yes = setTimeout(element.remove, 3000);
+        
+              function moving(){
+                timeout = setTimeout(stoptheleave, 1800);
+                ball.addEventListener("mouseleave", boxdeath);
+                ball.style.transform = "translateX(500px)";
+                timeout = setTimeout(score_perfect, 2000);
+              }
+        
+              function stoptheleave(){
+                element.removeEventListener("mouseleave", boxdeath);
+              }
+              function score_perfect(){
+                element.removeEventListener("mouseup",boxdeath);
+                element.addEventListener("mouseup",scoring);
+              }
+        
+              function scoring(){
+                head.innerHTML = "PERFECT";
+                head.classList.remove('animation'); 
+                  void head.offsetWidth;
+                  head.classList.add('animation');
+                    counter = counter + 10;
+                    ball.remove();
+                    element.remove();
+              }
+        
+              function scoreok(){
+                ball.remove();
+                element.remove();
+             head.innerHTML = "Ok?";
+             head.classList.remove('animation'); 
+            void head.offsetWidth;
+            head.classList.add('animation');
+          counter = counter + 1;
+              }
+        
+              function boxdeath(){
+                  ball.remove();
+                  element.remove();
+               head.innerHTML = "BAD";
+               head.classList.remove('animation'); 
+              void head.offsetWidth;
+              head.classList.add('animation');
+            counter = counter - 1;
+        
+              }
+                }
+    
 
 
 
